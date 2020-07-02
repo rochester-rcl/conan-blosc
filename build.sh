a632e05fe1f48485a74baded909ca5be66fb85f9
jjromphf <jromphf@library.rochester.edu>
2020-07-02 12:27:17 -0400

    added force to package export in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-mkdir src 
-mkdir build
 conan source . -sf src 
-conan install . -if build --build
+conan install . -if build --build missing
 conan build . -bf build -sf src
 conan export-pkg . Blosc/1.5.0@rcldsl/stable -s build_type=Release -sf src -bf build --force
